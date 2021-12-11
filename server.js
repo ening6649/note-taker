4b871737b6cf5feb2a98d587cee8602e40498af0
ening <ening6649@yahoo.com>
2021-12-11 11:13:17 -0500

    index.html path

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const path = require('path');
+
+const PORT = 3001;
+// instantiate the server
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
