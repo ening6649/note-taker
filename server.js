e81397b5b7aaceafd813e3326d49fc5456d34862
ening <ening6649@yahoo.com>
2021-12-11 12:20:25 -0500

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const path = require('path');
 const router = require('express').Router();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
 
@@ -11,14 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// router.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
